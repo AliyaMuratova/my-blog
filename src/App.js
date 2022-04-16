6cde61ed02185213439cdb6f1444cea593e04e65
Aliya Muratova <aliyahmuratova@gmail.com>
2022-04-16 20:30:36 +0600

    feat: added Header

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
 
+const App = () => {
+    return (
+        <div className="w3-content" style={{maxWidth: '1400px'}}>
+            <Header />
+
+        </div>
+    )
+}
+
+export default App;
