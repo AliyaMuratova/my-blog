f21698a3c192db207d814919b307fc9a1e86d433
Aliya Muratova <aliyahmuratova@gmail.com>
2022-05-25 15:30:31 +0600

    feat: added useState, useEffect, json-server

--- src/components/BlogEntry/BlogEntry.js
+++ src/components/BlogEntry/BlogEntry.js
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 
-const BlogEntry = (props) => {
+const BlogEntry = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    const fetchData = () => {
+        const url = `http://localhost:3002/posts`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setPosts(data))
+    };
+
+    useEffect(() => fetchData(), [])
+
     return (
         <>
-            <div className="w3-card-4 w3-margin w3-white">
-                <img src={props.imgUrl} alt="Nature" style={{width: '100%'}} />
-                <div className="w3-container">
-                    <h3><b>{props.title}</b></h3>
-                    <h5>{props.tags}, <span className="w3-opacity">{props.date}</span></h5>
-                </div>
-                <div className="w3-container">
-                    <p>{props.desc}</p>
-                    <div className="w3-row">
-                        <div className="w3-col m8 s12">
-                            <p><Link to="/post">
-                                <button className="w3-button w3-padding-large w3-white w3-border">
-                                    <b>READ MORE »</b>
-                                </button></Link>
-                            </p>
-                        </div>
-                        <div className="w3-col m4 w3-hide-small">
-                            <p><span className="w3-padding-large w3-right"><b>Comments &nbsp;</b> <span className="w3-tag">{props.countComments}</span></span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr />
+            {
+                posts.map((post, index) => {
+                    return (
+                        <>
+                            <div key={index} className="w3-card-4 w3-margin w3-white">
+                                <img src={post.img_url} alt="Nature" style={{width: '100%'}}/>
+                                <div className="w3-container">
+                                    <h3><b>{post.title}</b></h3>
+                                    <h5>{post.tags},<br/><span className="w3-opacity">{post.created_date}</span></h5>
+                                </div>
+                                <div className="w3-container">
+                                    <p>{post.desc}</p>
+                                    <div className="w3-row">
+                                        <div className="w3-col m8 s12">
+                                            <p><Link to="/post">
+                                                <button className="w3-button w3-padding-large w3-white w3-border">
+                                                    <b>READ MORE »</b>
+                                                </button>
+                                            </Link>
+                                            </p>
+                                        </div>
+                                        <div className="w3-col m4 w3-hide-small">
+                                            <p><span className="w3-padding-large w3-right"><b>Comments &nbsp;</b> <span
+                                                className="w3-tag">{post.count_comments}</span></span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr/>
+                        </>
+                    )
+                })
+            }
+
         </>
     );
 };
