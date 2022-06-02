67f44fc30bde7f74b815074fc8e2a32caf515899
Aliya Muratova <aliyahmuratova@gmail.com>
2022-06-02 21:24:05 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-import Post from "./pages/Post/Post";
-import Main from "./pages/Main/Main";
 import "./App.css"
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" exact={true} element={<Main />} />
-                <Route path="/post" element={<Post />} />
-            </Routes>
+                <Route path="/products" element={} />
         </BrowserRouter>
     )
 }
